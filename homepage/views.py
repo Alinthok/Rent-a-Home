d2e30dc2e726bb6b44cba454dda77821c133d5c6
alin fathul <alinthok@gmail.com>
2022-12-01 21:04:50 +0700

    added register page

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm  
+# Create your views here.  
+  
+def index(request):
+    return render(request, 'homepage/index.html') 
 
-# Create your views here.
+def login_page(request):
+    return render(request, 'homepage/login.html')  
+
+def register_page(request):  
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  
+        if form.is_valid():  
+            form.save() 
+            return redirect('/')
+    else:  
+        form = UserCreationForm()  
+    context = {  
+        'form':form  
+    }  
+    return render(request, 'homepage/register.html', context)  
